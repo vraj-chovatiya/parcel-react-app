fc357c53856a725f3cf1efd8b57a427658e8c938
vraj_chovatiya <vrajchovatiya414@gmail.com>
2024-06-08 16:09:31 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./src/components/Header";
+
+const App = () => {
+    return (
+        <>
+            <div>
+                <Header />
+                <h1>render all the component here...</h1>
+                <p>hello sir, it's done...</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header Component</h1>
+            <p>hello sir, it's done...</p>
+        </div>
+    );
+}
+
+export default Header;
